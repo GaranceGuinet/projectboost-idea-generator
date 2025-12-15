4e3ef7a3c78689df4b89cef3719079ada623a395
GaranceGuinet <guinetgarance@laposte.net>
2025-12-15 18:26:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# ProjectBoost
+
+## Description
+
+ProjectBoost est un générateur d'idées de projets web destiné aux développeurs débutants.
+En un clic, l'utilisateur obtient une idée de projet à réaliser afin de s'entraîner en développement web.
+
+## Fonctionnalités
+
+- Génération aléatoire d'idées de projets web
+- Empêche l'affichage de la même idée deux fois de suite
+- Bouton désactivé temporairement pendant la génération
+- Message de chargement simulé ("Génération en cours...")
+
+## Technologie utilisées 
+
+- HTML5
+- CSS3
+- JavaScript
+
+## Comment utiliser le projet
+
+1. Cloner le dépôt ou télécharger les fichiers
+2. Ouvrir le fichier `index.html` dans un navigateur
+3. Cliquer sur le bouton sur le boutton **Générer une idée de projet"**
+
+## Objectif pédagogique
+
+Ce projet a été réalisé dans un but pédagogique afin de pratiquer :
+
+- La manipulation du DOM en JavaScript 
+- La gestion des évenements ( `addEventListener` )
+- L'utilisation des tableaux
+- La génération de valeurs aléatoires
+- La gestion d'un état (éviter les doublons)
+- Les fonctions asynchrones avec `setTimeout`
---
+++ script.js
@@ -0,0 +1,44 @@
+// Tableau contenant toutes les idées de projets à afficher aléatoirement
+const ideas = [
+  "Créer un site vitrine pour un freelance",
+  "Développer une to-do list simple en java-script",
+  "Créer un générateur de citations",
+  "Réaliser une page portfolio personnelle",
+  "Créer un formulaire de contact avec validation java-script",
+  "Développer un mini jeu de devinettes",
+  "Créer un convertisseur de devises",
+  "Créer une page FAQ interactive",
+  "Développer un compteur interactif",
+  "Créer un générateur de mots de passe simples"
+]
+
+// Récupération des éléments du DOM (zone d'affichage et bouton)
+const ideaText = document.getElementById("idea")
+const generateBtn = document.getElementById("generate-btn")
+
+// Stocke l'index de la dernière idée affichée
+let lastIndex = null
+// Contiendra l'index de l'idée générée aléatoirement
+let randomIndex
+
+ // Génération d'une nouvelle idée au clic sur le bouton
+generateBtn.addEventListener("click", () => {
+    // Désactive le bouton et affiche un message pendant la génération
+    generateBtn.disabled = true
+    ideaText.textContent = "Génération en cours..."
+    // Délai artificiel pour simuler un temps de génération
+    setTimeout (() => {
+    // Génère un index aléatoire différent du précédent
+do {
+    randomIndex = Math.floor(Math.random() * ideas.length)
+} 
+    while (randomIndex === lastIndex)
+    // Affiche l'idée selectionnée
+    ideaText.textContent = ideas[randomIndex]
+    // Mémorise l'index pour le prochain clic
+     lastIndex = randomIndex
+    // Réactive le bouton après la génération
+    generateBtn.disabled = false
+}, 500)
+
+})
